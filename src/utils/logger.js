646d75c489c9f9c777fbd164352c5f9c18620c11
shaysiboni1-alt <shaysiboni1@gmail.com>
2026-01-31 05:43:07 +0200

    Update logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,1 +1,19 @@
+// src/utils/logger.js
+function base(level, msg, meta) {
+  const time = new Date().toISOString();
+  if (meta !== undefined) {
+    console.log(JSON.stringify({ time, level, msg, meta }));
+  } else {
+    console.log(JSON.stringify({ time, level, msg }));
+  }
+}
+
+const logger = {
+  info: (msg, meta) => base("info", msg, meta),
+  warn: (msg, meta) => base("warn", msg, meta),
+  error: (msg, meta) => base("error", msg, meta),
+  debug: (msg, meta) => base("debug", msg, meta)
+};
+
+module.exports = { logger };
 
