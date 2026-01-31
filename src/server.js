d3409d9e000a01573911785ef2e1a25afcf99798
shaysiboni1-alt <shaysiboni1@gmail.com>
2026-01-31 19:47:59 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,14 +3,12 @@
 
 const http = require("http");
 const express = require("express");
-const { WebSocketServer } = require("ws");
-
 const { env } = require("./config/env");
 const { logger } = require("./utils/logger");
 const { healthRouter } = require("./routes/health");
 const { adminReloadRouter } = require("./routes/adminReloadSheets");
 const { loadSSOT } = require("./ssot/ssotClient");
-const { handleTwilioMediaWs } = require("./telephony/twilioMediaStream");
+const { attachTwilioMediaWs } = require("./telephony/twilioMediaWs");
 
 const app = express();
 app.use(express.json({ limit: "1mb" }));
@@ -22,17 +20,19 @@ app.use((req, res) => res.status(404).json({ error: "not_found" }));
 
 const server = http.createServer(app);
 
-// WS server for Twilio Media Streams
-const wss = new WebSocketServer({ server, path: "/twilio-media-stream" });
-wss.on("connection", (ws) => handleTwilioMediaWs(ws));
+// WebSocket: /twilio-media-stream
+attachTwilioMediaWs(server);
 
 server.listen(env.PORT, async () => {
-  logger.info("Service started", { port: env.PORT, provider_mode: env.PROVIDER_MODE });
+  logger.info("Service started", {
+    port: env.PORT,
+    provider_mode: env.PROVIDER_MODE,
+  });
 
   // Best-effort preload SSOT
   try {
     await loadSSOT(false);
   } catch (err) {
-    logger.error("SSOT preload failed", { error: err && err.message ? err.message : String(err) });
+    logger.error("SSOT preload failed", { error: err.message });
   }
 });
