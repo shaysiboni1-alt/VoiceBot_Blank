77df541bf96ded42b8cfc60b2e06dee1b49740f3
shaysiboni1-alt <shaysiboni1@gmail.com>
2026-02-10 17:37:31 +0200

    Update twilioMediaWs.js

--- src/ws/twilioMediaWs.js
+++ src/ws/twilioMediaWs.js
@@ -41,7 +41,8 @@ function installTwilioMediaWs(server) {
       } catch {}
     }
 
-    twilioWs.on("message", (data) => {
+    // ✅ FIX: callback is async so "await" is legal
+    twilioWs.on("message", async (data) => {
       let msg;
       try {
         msg = JSON.parse(data.toString("utf8"));
